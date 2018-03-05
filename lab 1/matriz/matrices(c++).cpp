8a9a71699387bb9f0c957d804eee01256cd5a92b
Brayan <bariascosr@unal.edu.co>
2018-03-04 20:43:03 -0500

    Primer commit

--- lab 1/matriz/matrices(c++).cpp
+++ lab 1/matriz/matrices(c++).cpp
@@ -38,9 +38,9 @@ double tiempo_inicial, tiempo_final, suma = 0.0;
 for (int x = 0; x < 10; x++) 
 {
 tiempo_inicial = clock();
-for (i = 0; i <= n; i++){
-    for (j = 0; j <= n; j++){
-      for (k = 0; k <= n; k++){
+for (i = 0; i < n; i++){
+    for (j = 0; j < n; j++){
+      for (k = 0; k < n; k++){
         matrizf[i][j] += matriz[i][k] * matriz2[k][j];
       }
     }
